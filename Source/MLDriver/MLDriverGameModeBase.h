b31f4da11e3f897f6a47c29d05cf499241046aac
H36615 <h36615@protonmail.com>
2020-06-16 00:34:16 +0300

    Add some default project files

---
+++ Source/MLDriver/MLDriverGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MLDriverGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MLDRIVER_API AMLDriverGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
